0a6abe28b7cd67d0f0943913209e691d9fead92b
couper117 <ikennykelvin75@gmail.com>
2026-01-26 04:33:11 -0800

    new user

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // start server
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
@@ -24,13 +24,44 @@ const students = [
   },
 ];
 
+let users = [
+  { id:1, name:"john", email:"john@gmail.com", password:"123456"}
+]
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  if (!newUser.password || newUser.password.length < 6) { 
+    return res.status(400).send("Error: Password is required and must be at least 6 characters long");
+  } 
+  if (!newUser.email || !newUser.email.includes("@")) {
+    return res.status(400).send("Error: Valid email is required");
+  }
+  
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send({"User added" });
+});
+
+
 app.get("/students", (req, res) => {
   res.json(students);
 });
 app.post("/students", (req, res) => {
-  const newStudent = req.body;
+ const newstudent = req.body;
+   if (!newstudent.name || typeof newstudent.name !== "string") {
+    return res.status(400).send("Error: Name is required and must be a string");
+  }
+  if (!newstudent.grade || typeof newstudent.grade !== "number") {
+    return res.status(400).send("Error: Grade is required and must be a number");
+  }
+
+  newStudent.id = students.length +1;
   students.push(newStudent);
-  res.status(201).send("student added");
+  res.status(201).send({message: "student added" , students: newStudent});
 });
 
 
